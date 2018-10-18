1afba4020bc98a51e323118555e8c489ee815f69
Son Dong Hwan <zxcehdghks@gmail.com>
2018-10-18 14:46:36 +0900

    static() 함수를 사용해서 MEDIA_URL, MEDIA_ROOT처리

--- app/config/urls.py
+++ app/config/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -20,5 +22,10 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('media/<path:path>', views.media_serve),
 ]
+# MEDIA_URL로 시작하는 URLs static()내의serve() 함수를 통해 처리
+# MEDIA_ROOT 기준으로 파일을 검색함
+urlpatterns += static(
+    prefix=settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
