7d9d65bceccee0e5419de2d6ec12413dd8852afd
Son Dong Hwan <zxcehdghks@gmail.com>
2018-10-22 12:12:50 +0900

    Post, Comment에서 Django의 User모델을 사용

--- app/posts/models.py
+++ app/posts/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
-from members.models import User
+from django.conf import settings
 
 
 class Post(models.Model):
     author = models.ForeignKey(
-        # <AppName>.<ModelName>
-        # 'members.User',
-        User,
+        # 'auth.User'
+        #  Django가 기본으로 제공하는 User클래스
+        settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         verbose_name='작성자',
     )
     photo = models.ImageField(
         '사진',
-        upload_to='post'
+        upload_to='post',
     )
     # auto_now_add: 객체가 처음 생성될때의 시간 저장
     # auto_now: 객체의 save()가 호출될 때 마다 시간 저장
@@ -32,20 +32,16 @@ class Comment(models.Model):
         on_delete=models.CASCADE,
         verbose_name='포스트',
     )
-
     author = models.ForeignKey(
-        'members.User',
+        settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         verbose_name='작성자',
     )
-
-    content = models.TextField(
-        '댓글 내용',
-    )
+    content = models.TextField('댓글 내용')
     tags = models.ManyToManyField(
         'HashTag',
         blank=True,
-        verbose_name='해쉬태그 목록',
+        verbose_name='해시태그 목록',
     )
 
     class Meta:
@@ -54,20 +50,11 @@ class Comment(models.Model):
 
 
 class HashTag(models.Model):
-    name = models.CharField(
-        '태그명',
-        max_length=100
-    )
+    name = models.CharField('태그명', max_length=100)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = '해쉬태그'
+        verbose_name = '해시태그'
         verbose_name_plural = f'{verbose_name} 목록'
-
-
-
-class UploadFileModel(models.Model):
-    title = models.TextField(default='')
-    file = models.FileField(null=True)
--- app/posts/views.py
+++ app/posts/views.py
@@ -1,79 +1,76 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 
-from members.models import User
 from .models import Post
 from .forms import PostCreateForm
 
 
 def post_list(request):
     # 1. Post모델에
-    # created_at (생성시간 저장)
-    #   auto_now_add=True
-    # modified_at (수정시간 저장)
-    #   auto_now = True
-    # 두 필드를 추가
+    #  created_at (생성시간 저장)
+    #       auto_now_add=True
+    #  modified_at (수정시간 저장)
+    #       auto_now=True
+    #   두 필드를 추가
 
     # 2. Post모델이 기본적으로 pk내림차순으로 정렬되도록 설정
-    #       class Meta:
-    #           ordering = ['-pk']
+    #     class Meta:
+    #         ordering = ['-pk']
 
-    # 3. 모든 Post 객체에 대한 QuerySet을
-    #     render의 context 인수로 전달
-    #       context = {'posts': Post.objects.all()}
+    # 3. 모든 Post객체에 대한 QuerySet을
+    #    render의 context인수로 전달 (키: posts)
+    #      context = {'posts': Post.objects.all()}
 
     # 4. posts/post_list.html을 Template으로 사용
-    #     템플릿 에서는 posts값을 순회하며
+    #     템플릿에서는 posts값을 순회하며
     #     각 Post의 photo정보를 출력
 
     # 5. url은 posts.urls모듈을 사용
-    #       config.urls에서 해당 모듈을 include
-    #       posts.urls.app_name = 'posts'를 사용
-    #       include할 URL은 'posts/'
-    #       view의 URL은 비워둔다
-    #       결과: localhost:8000/posts/로 접근시 이 view가 처리하도록 함
+    #    config.urls에서 해당 모듈을 include
+    #    posts.urls.app_name = 'posts'를 사용
+    #     include할 URL은 'posts/'
+    #     view의 URL은 비워둔다
+    #      결과: localhost:8000/posts/ 로 접근시
+    #            이 view가 처리하도록 함
     posts = Post.objects.all()
     context = {
         'posts': posts,
     }
     return render(request, 'posts/post_list.html', context)
 
-def handle_uploaded_file(f):
-    with open('some/file/name.txt', 'wb+') as destination:
-        for chunk in f.chunks():
-            destination.write(chunk)
-
 
 def post_create(request):
     # 1. posts/post_create.html 구현
-    # form 구현
+    #  form구현
     #   input[type=file]
     #   button[type=submit]
-    
+
     # 2. /posts/create/ URL에 이 view를 연결
-    #         URL명은 'post-create'를 사용
+    #    URL명은 'post-create'를 사용
     # 3. render를 적절히 사용해서 해당 템플릿을 return
     # 4. base.html의 nav부분에 '+ Add Post'텍스트를 갖는 a링크 하나 추가,
-    #       {% url %} 태그를 사용해서 포스트 생성 으로 링크 걸어주기
+    #     {% url %} 태그를 사용해서 포스트 생성 으로 링크 걸어주기
+
     if request.method == 'POST':
-        # request.FILES에 form에서 보낸 파일객체가 들어 있음
+        # request.FILES에 form에서 보낸 파일객체가 들어있음
         # 새로운 Post를 생성한다.
-        # author는 User.objects.first()
-        # photo는 request.FILES에 있는 내용을 적절히 꺼내서 사용
+        #  author는 User.objects.first()
+        #  photo는 request.FILES에 있는 내용을 적절히 꺼내서 쓴다
         # 완료된 후 posts:post-list로 redirect
         post = Post(
-            author=User.objects.first(),
+            # SessionMiddleware
+            # AuthenticationMiddleware
+            #  를 통해서 request의 user속성에
+            #  해당 사용자 인스턴스가 할당
+            author=request.user,
             photo=request.FILES['photo'],
         )
         post.save()
         return redirect('posts:post-list')
     else:
-        # GET 요청의 경우, 빈 Form인스턴스를 context에 담아서 전달
-        # Template에서는 'form' 키로 해당 Form인스턴스 속성을 사용 가능
+        # GET요청의 경우, 빈 Form인스턴스를 context에 담아서 전달
+        # Template에서는 'form'키로 해당 Form인스턴스 속성을 사용 가능
         form = PostCreateForm()
         context = {
             'form': form,
         }
         return render(request, 'posts/post_create.html', context)
-
-
