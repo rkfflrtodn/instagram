96fd65e07ff153f24428935b9593626398796a2c
Son Dong Hwan <zxcehdghks@gmail.com>
2018-10-22 12:26:42 +0900

    post_create에 접근했는데 로그인 되어 있지 않으면 post_list로 redirect처리

--- app/members/models.py
+++ app/members/models.py
@@ -1,6 +1,5 @@
 # from django.db import models
 #
-#
 # class User(models.Model):
 #     username = models.CharField(
 #         '아이디',
--- app/posts/views.py
+++ app/posts/views.py
@@ -39,16 +39,9 @@ def post_list(request):
 
 
 def post_create(request):
-    # 1. posts/post_create.html 구현
-    #  form구현
-    #   input[type=file]
-    #   button[type=submit]
+    if not request.user.is_authenticated:
+        return redirect('posts:post-list')
 
-    # 2. /posts/create/ URL에 이 view를 연결
-    #    URL명은 'post-create'를 사용
-    # 3. render를 적절히 사용해서 해당 템플릿을 return
-    # 4. base.html의 nav부분에 '+ Add Post'텍스트를 갖는 a링크 하나 추가,
-    #     {% url %} 태그를 사용해서 포스트 생성 으로 링크 걸어주기
 
     if request.method == 'POST':
         # request.FILES에 form에서 보낸 파일객체가 들어있음
