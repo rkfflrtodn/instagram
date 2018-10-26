9361471697c9b168432570b0695d67bbeb06290d
Son Dong Hwan <zxcehdghks@gmail.com>
2018-10-26 15:55:34 +0900

    img_profile이 비어있는 경우를 property를 사용해서 예외처리

--- app/members/models.py
+++ app/members/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.db import models
+
 
 class User(AbstractUser):
     img_profile = models.ImageField(
@@ -20,3 +22,12 @@ class User(AbstractUser):
     class Meta:
         verbose_name = '사용자'
         verbose_name_plural = f'{verbose_name} 목록'
+
+    @property
+    def img_profile_url(self):
+        # 자신의 img_profile필드에 내용이 있다면
+        # 해당 파일로 접근할 수 있는 url을 리턴
+        if self.img_profile:
+            return self.img_profile.url
+        # 없다면 static폴더에서 뒷 경로 파일의 url을 리턴
+        return static('images/blank_user.png')
