495092e11b4e8933acd0fd759178f94a9dba9ef4
Son Dong Hwan <zxcehdghks@gmail.com>
2018-10-30 12:23:52 +0900

    Facebooklogin완료

--- app/members/views.py
+++ app/members/views.py
@@ -1,13 +1,15 @@
-import json
+import imghdr
 
 import requests
 from django.contrib import messages
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, get_user_model
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.shortcuts import render, redirect
 
 from .forms import LoginForm, SignupForm, UserProfileForm
+
+User = get_user_model()
 
 
 def login_view(request):
@@ -156,7 +158,9 @@ def profile(request):
 
 
 def facebook_login(request):
-    api_get_access_token = 'https://graph.facebook.com/v3.2/oauth/access_token?'
+    api_base = 'https://graph.facebook.com/v3.2'
+    api_get_access_token = f'{api_base}/oauth/access_token?'
+    api_me = f'{api_base}/me'
     # URL: /members/facebook-login/
     # URL name: 'members:facebook-login'
     # request.GET에 전달된 'code'값을
@@ -186,4 +190,50 @@ def facebook_login(request):
     access_token = data['access_token']
 
     # access_token을 사용해서 사용자 정보를 가져오기
-    pass
+    params = {
+        'access_token': access_token,
+        'fields': ','.join([
+            'id',
+            'first_name',
+            'last_name',
+            'picture.type(large)',
+        ]),
+    }
+    response = requests.get(api_me, params)
+    data = response.json()
+
+    facebook_id = data['id']
+    first_name = data['first_name']
+    last_name = data['last_name']
+    url_img_profile = data['picture']['data']['url']
+    # HTTP GET요청의 응답을 받아와서 binary data를 img_data변수에 할당
+    img_response = requests.get(url_img_profile)
+    img_data = img_response.content
+
+    # 응답의 binary data를 사용해서 In-memory binary stream(file)객체를 생성
+    # 이렇게 안하고 FileField가 지원하는 InMemoryUploadedFile객체를 사용하기!
+    # f = io.BytesIO(img_response.content)
+
+    # imghdr모듈을 사용해 Image binary data의 확장자를 알아냄
+    ext = imghdr.what('', h=img_data)
+    # Form에서 업로드한 것과 같은 형태의 file-like object생성
+    #  첫 번째 인수로 반드시 파일명이 필요. <facebook_id>.<확장자>형태의 파일명을 지정
+    f = SimpleUploadedFile(f'{facebook_id}.{ext}', img_response.content)
+
+    try:
+        user = User.objects.get(username=facebook_id)
+        # update_or_create
+        user.last_name = last_name
+        user.first_name = first_name
+        # user.img_profile = f
+        user.save()
+    except User.DoesNotExist:
+        user = User.objects.create_user(
+            username=facebook_id,
+            first_name=first_name,
+            last_name=last_name,
+            img_profile=f,
+        )
+
+    login(request, user)
+    return redirect('posts:post-list')
